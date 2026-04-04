2048b436b0df6c15790d52f76355f0b13be4c1bf
stoleckipawel <stoleckipawel98@gmail.com>
2026-04-04 20:16:03 +0200

    Revert "asset converter as seperate tool"
    This reverts commit b610b28ce7514392a2830664998097434de1168a.

--- Tools/AssetConverter/GltfImporter.cpp
+++
@@ -1,8 +0,0 @@
-#include "GltfImporter.h"
-
-#include "GameFramework/Public/Assets/GltfLoader.h"
-
-SceneImportResult GltfImporter::Load(const std::filesystem::path& filePath)
-{
-	return GltfLoader::Load(filePath);
-}
